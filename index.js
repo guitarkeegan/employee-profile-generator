920410dee59aaa9082ae86c66b4ec9c208619614
guitarkeegan <keegananglim@gmail.com>
2022-06-21 08:57:55 -0700

    add prompt for manager questions, and outline for the logic to follow

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const {Employee, Manager, Engineer, Intern} = require("./lib/classes");
 
-questions = [
+managerQuestions = [
     {
         type: "input",
         name: "managerName",
@@ -19,21 +20,57 @@ questions = [
     },
     {
         type: "input",
-        name: "managerName",
-        message: "Who is the team manager? "
-    },
-    {
-        type: "input",
         name: "officeNumber",
         message: "What is their office number? "
-    },
+    }
+]
+const moreQuestions = [
     {
         type: "list",
         name: "addTeamMember",
         message: "Would you like to add a team member? ",
         choices: ["add engineer", "add intern", "I'm finished building the team"]
-    },
+    }
 ]
+const initialQuestions = managerQuestions.splice(3)
+const engineerQuestion = [
+    [...initialQuestions],
+    {
+        type: "input",
+        name: "github",
+        message: "What is their github username? "
+    }
+]
+const internQuestions = [
+    [...initialQuestions],
+    {
+        type: "input",
+        name: "school",
+        message: "Which school is the intern attending? "
+    }
+]
+inquirer.prompt(managerQuestions)
+.then(answers=>{
+    const {managerName, id, email, officeNumber} = answers;
+    const manager = new Manager(managerName, id, email, officeNumber)
+    // send to html generator
+});
+
+inquirer.prompt(moreQuestions)
+.then(answers=>{
+    if (answers.addTeamMember === "add engineer"){
+        engineerSetup() // will call the prompt with engineer questions
+    } else if (answers.addTeamMember === "add intern"){
+        internSetup() // call prompt with intern questions
+    } else {
+        // create html
+    }
+})
+
+// TODO: export variables to template-helper.js
+// TODO: Create engineerSetup and InternSetup functions
+
+
 
 
 // GIVEN a command-line application that accepts user input
