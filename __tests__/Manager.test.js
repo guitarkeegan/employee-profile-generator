5440ea10e4d7941b462edb5efb388012dac76988
guitarkeegan <keegananglim@gmail.com>
2022-06-22 20:49:22 -0700

    add getOfficeNumber(), tests passing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,5 +1,5 @@
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
+const { Manager } = require("../lib/classes");
+const { Employee } = require("../lib/classes");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
